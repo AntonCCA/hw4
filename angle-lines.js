24f3503c2be6c437831758fd7970a84d47197886
Anton Castro <47227338+AntonCCA@users.noreply.github.com>
2019-02-23 15:45:20 -0800

    Create angle-lines.js

---
+++ angle-lines.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+}
+
+function draw() {
+  background(255);
+
+  for (var x = 25; x < width - 1; x = x + 6) {
+    line(x, height/2, x+100, height/2-75);
+    
+  }
+}
+
+ 
